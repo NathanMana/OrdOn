db3cf54b5fa8bb8b11f8d7207df21a2a9f52768d
RomainEfrei <romain.berthon@efrei.net>
2021-06-17 18:41:13 +0200

    Squelette du code

---
+++ OrdON/serveur.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+// dependence installer: nodemon, express, ejs, bcrypt
+// pour lancer le serveur npm run DevStart ( pas besoin de restart apres avoir save nodemon s en charge )
+app.set('view engine', 'ejs')
+
+
+/*app.get('/', (req, res) =>{
+
+})*/
+
+
+
+
+
+app.listen(8000)

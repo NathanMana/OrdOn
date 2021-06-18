c5ec84703b5f16ee95345974c9b563aef535f4f9
RomainEfrei <romain.berthon@efrei.net>
2021-06-18 12:22:51 +0200

    Sauvegarde de la création de prescription, il manque uniquement la validation

--- OrdON/src/js/createPrescription.js
+++ OrdON/src/js/createPrescription.js
@@ -1,5 +1,5 @@
 function addLine(){
-    var prescriptionDiv = document.getElementById("prescription");
+    var prescriptionDiv = document.getElementById("prescriptionLines");
 
     var newLine = document.createElement("div")
     newLine.className = "prescriptionLine"
@@ -8,16 +8,19 @@ function addLine(){
     newInputName.type = "text";
     newInputName.name = "medicName";
     newInputName.required = true;
+    newInputName.id = "nameInput"
 
     var newInputDescription = document.createElement("input");
     newInputDescription.type = "text";
     newInputDescription.name = "medicDescription";
     newInputDescription.required = true;
+    newInputDescription.id = "descriptionInput"
 
     var newInputMention = document.createElement("input");
     newInputMention.type = "text";
     newInputMention.name = "medicMention";
     newInputMention.required = true;
+    newInputMention.id = "mentionInput"
 
     newLine.appendChild(newInputName);
     newLine.appendChild(newInputDescription);

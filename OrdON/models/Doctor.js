7346a4002221a1ff35820332f7a8e1040e6982e7
Nathan Manaranche <nathan.manaranche@gmail.com>
2021-06-22 14:06:20 +0200

    Correction .ejs.html en .ejs

--- OrdON/models/Doctor.js
+++ OrdON/models/Doctor.js
@@ -4,6 +4,7 @@ class Doctor extends Profesionnal {
     constructor(id_doctor) {
         super(Profesionnal);
         this.id_doctor = id_doctor
+        
     }
 
     getId() {return this.id_doctor}

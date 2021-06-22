2831ce9e17ecaf0ee897f8888f0c989f0bd68837
Nathan Manaranche <nathan.manaranche@gmail.com>
2021-06-22 10:58:52 +0200

    Correction responsive page d'accueil

---
+++ OrdON/services/TestServices.js
@@ -0,0 +1,10 @@
+TestServices = function() {
+    this.name = "Jean"
+    this.test = "test"
+    this.createUser = function(name)
+    {
+        console.log(name)
+    }
+}
+
+module.exports = TestServices

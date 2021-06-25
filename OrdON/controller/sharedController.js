7828da13d5d8c9089948c6d1616738cb146f1b01
jean-ludovic <37267183+Gaartner@users.noreply.github.com>
2021-06-25 20:24:27 +0200

    add all route connection and register

--- OrdON/controller/sharedController.js
+++ OrdON/controller/sharedController.js
@@ -10,20 +10,59 @@ const PatientServices = require('../services/PatientServices')
 router.get('/', async (req, res) => {
     res.render('index')
 })
-
+/**
+ * Gère l'affichage de la page de la connexion avec les 3 statuts
+ */
 router.get('/connexion', (req, res)=>{
     res.render('connectionPath')
 })
-
-router.get('/pharmacien', (req, res)=>{
+/**
+ * Gère l'affichage de la page de la connexion avec le formulaire 
+ */
+router.get('/pharmacist/connectionPharmacist', (req, res)=>{
     res.render('../views/Pharmacist/connectionPharmacist')
 })
-
-router.get('/patient', (req, res)=>{
-    res.render('connectionPath')
+/**
+ * Gère l'affichage de la page de la connexion avec le formulaire 
+ */
+router.get('/patient/connectionPatient', (req, res)=>{
+    res.render('../views/Patient/connectionPatient')
+})
+/**
+ * Gère l'affichage de la page de la connexion avec le formulaire 
+ */
+router.get('/doctor/connectionDoctor', (req, res)=>{
+    res.render('../views/Doctor/connectionDoctor')
 })
 
-router.get('/docteur', (req, res)=>{
-    res.render('connectionPath')
+
+/**
+ * Gère l'affichage de la page de l'inscription avec les 3 status 
+ */
+
+
+ router.get('/register',(req, res)=>{
+    res.render('registerPath')
 })
+/**
+ * Gère l'affichage de la page de la connexion avec le formulaire 
+ */
+ router.get('/pharmacist/registerPharmacist', (req, res)=>{
+    res.render('../views/Pharmacist/registerPharmacist')
+})
+/**
+ * Gère l'affichage de la page de la connexion avec le formulaire 
+ */
+router.get('/patient/registerPatient', (req, res)=>{
+    res.render('../views/Patient/registerPatient')
+})
+/**
+ * Gère l'affichage de la page de la connexion avec le formulaire 
+ */
+router.get('/doctor/registerDoctor', (req, res)=>{
+    res.render('../views/Doctor/registerDoctor')
+})
+/**
+ * Gère l'affichage de la page de l'inscription avec les 3 status 
+ */
 module.exports = router

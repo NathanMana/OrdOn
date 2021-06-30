17a89c914db893386aee1b4d63194f4cd362614d
MrAlphajet <yannick.yanat@efrei.net>
2021-06-30 16:54:50 +0200

    ajax pour romain

--- OrdON/src/js/recupQrcodeOrdonnance.js
+++ OrdON/src/js/recupQrcodeOrdonnance.js
@@ -1,13 +1,16 @@
-const { JSDOM } = require( "jsdom" );
-const { window } = new JSDOM( "" );
-const $ = require( "jquery" )( window );
+// const { JSDOM } = require( "jsdom");
+// const { window } = new JSDOM( "" );
+// const $ = require( "jquery" )( window );
 
 
 function getQRcode() {
+    console.log("Appel de get QR");
+
     $.ajax({
-        url : '/getordonnance',
+        url : '/patient/getordonnance',
         type : 'GET',
         success : function(res){
+            console.log(res)
             var h3 = document.createElement('h3');
             h3.id="qrPatientText"
             h3.textContent = "Votre QR Code patient"
@@ -17,11 +20,30 @@ function getQRcode() {
             var p = document.createElement('p')
             p.className = "textCenter";
             p.onClick = "closeQR()";
-            var qrcode = document.createElement('div').appendChild(h3)
+            var qrcode = document.getElementById('QRCodeDiv').appendChild(h3)
             qrcode.appendChild(img)
             qrcode.appendChild(p)
             document.querySelector('body').appendChild(qrcode)
 
         }
     });
+}
+
+
+function showQR(){
+    getQRcode()
+
+    var qrDiv = document.getElementById("QRCodeDiv");
+    qrDiv.style.display = "block";
+
+    /*var basePage = document.getElementById("coverPage");
+    basePage.style.display = "block"*/
+}
+
+function closeQR(){
+    var qrDiv = document.getElementById("QRCodeDiv");
+    qrDiv.style.display = "none";
+
+    var basePage = document.getElementById("coverPage");
+    basePage.style.display = "none"
 }

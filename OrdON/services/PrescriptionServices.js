d4e816042f10de3f6ec118ce185a3c0b3321398e
yalayl <loic.autret@eramet.com>
2021-06-30 17:49:19 +0200

    modif displayPrescription

--- OrdON/services/PrescriptionServices.js
+++ OrdON/services/PrescriptionServices.js
@@ -59,13 +59,14 @@ class PrescriptionServices {
      /**
      * @returns {listPrescription} la liste d'ordonnance
      */
-      static async displayPrescriptionPatient(){
+      static async displayPrescriptionPatient(id_patient){
         try {
-            listPrescription = []
+            listPrescriptionNoneArchived = []
+            listePrescriptionArchived = []
             const connection = await pool.getConnection();
             const result = await connection.query(
-                'SELECT * FROM prescription WHERE id_patient= ? ORDER BY date_archive',
-                [req.session.id], function(err,rows){
+                'SELECT * FROM prescription WHERE id_patient= ? AND date_archived IS NULL',
+                [id_patient], function(err,rows){
                     rows.forEach(element => {
                         // On convertit le résultat en objet js
                         const prescription = new Prescription()
@@ -73,12 +74,26 @@ class PrescriptionServices {
                         //On complete l'objet prescription avec les Attributions et les conseils
                         prescription.setListAttributions(AttributionService.getListAttributionsByPrescriptionId(prescription.getIdPrescription()))
                         prescription.setListCouncils(CouncilService.getListCouncilsByPrescriptionId(prescription.getIdPrescription()))
-                        listPrescription.add(prescription)
+                        listPrescriptionNoneArchived.add(prescription)
                     });    
                 })
-            if (!result) throw 'Une erreur est survenue'
+                if (!result) throw 'Une erreur est survenue'
+                const result2 = await connection.query(
+                    'SELECT * FROM prescription WHERE id_patient= ? AND date_archived IS NOT NULL',
+                    [id_patient], function(err,rows){
+                        rows.forEach(element => {
+                            // On convertit le résultat en objet js
+                            const prescription = new Prescription()
+                            Object.assign(prescription, element[0][0])
+                            //On complete l'objet prescription avec les Attributions et les conseils
+                            prescription.setListAttributions(AttributionService.getListAttributionsByPrescriptionId(prescription.getIdPrescription()))
+                            prescription.setListCouncils(CouncilService.getListCouncilsByPrescriptionId(prescription.getIdPrescription()))
+                            listPrescriptionArchived.add(prescription)
+                        });    
+                    })
+            if (!result2) throw 'Une erreur est survenue'
             connection.release()
-            return listPrescription
+            return listPrescriptionNoneArchived, listePrescriptionArchived
         }catch(e){ console.log(e)}     
     }
 
